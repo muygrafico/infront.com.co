04a2b5f6dcffe8a8a6910083b5b224fe42fc9612
German Hernandez <muygrafico@gmail.com>
2016-02-10 15:40:23 -0500

    form working

--- site/js/modules/form-google-docs.js
+++ site/js/modules/form-google-docs.js
@@ -3,7 +3,8 @@ console.log('load form js');
 var request;
 var scriptURL = "https://script.google.com/macros/s/AKfycby_ZWVM0ao9tfYBTMSrO_v_H22QPy2pl7st_ybX457kmZeXqh8/exec";
 // Bind to the submit event of our form
-$("#foo").submit(function(event){
+
+$("#registro form").submit(function(event){
 
     // Abort any pending request
     if (request) {
@@ -37,11 +38,22 @@ $("#foo").submit(function(event){
         console.log(response);
         console.log(textStatus);
         console.log(jqXHR);
+
+        var form_name = $('#name').val();
+
+        $("#registro form").remove();
+				$("#registro .contact-container").append('<div class="success"><h3 class="name">'+ form_name +'</h3><br><p>¡Gracias por Pre-Registrarte a inFront!<hr/></p><p class="copy">!Muy pronto nos pondremos en contacto para darte acceso a tus 2 meses de Prueba Grátis!</p></div>');
+
     });
 
     // Callback handler that will be called on failure
     request.fail(function (jqXHR, textStatus, errorThrown){
         // Log the error to the console
+
+
+        $("#registro form").remove();
+				$("#registro .contact-container").append('<div class="success"><h3 class="name">'+ form_name +'</h3><br><p>Parece que hubo un error<hr/></p><p class="copy">Porfavor intenta de nuevo recargando la página, si el problema persiste porfavor escribenos a info@infront.com</p></div>');
+
         console.error(
             "The following error occurred: "+
             textStatus, errorThrown
